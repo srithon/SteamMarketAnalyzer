eaef561ff79e1ddca794fca0ae975404436c32ee
srithon <36751023+srithon@users.noreply.github.com>
2019-07-19 22:56:32 -0700

    Replaced weighted average function

--- analyzeData.hs
+++ analyzeData.hs
@@ -4,16 +4,12 @@ This function returns an average of a list
 such that the values towards the beginning are
 given higher weighting than those at the end
 -}
-weightedAverage' :: Double -> Double -> [Int] -> Double
-weightedAverage' x y [] = 0
-weightedAverage' currentWeight interval (x:xs) = (fromIntegral(x) * (currentWeight) + (weightedAverage' (currentWeight - interval) interval xs))
-
-weightedAverage :: [Int] -> Double
-weightedAverage (x:xs) = (weightedAverage' (1 - (weightingInterval $ (length xs + 1))) (weightingInterval (length xs + 1)) (x:xs)) / (fromIntegral(length xs + 1) / 2)
-
 -- wI 5; 0.4 0.25 0.125 
 weightingInterval :: Int -> Double
 weightingInterval n = (fromIntegral 1) / fromIntegral (n + 1)
+
+weightedAverage :: [Double] -> Double
+weightedAverage (x:xs) = (sum $ (zipWith(*) (x:xs) [1 - weightingInterval ((length xs) + 1), 1 - 2 * (weightingInterval ((length xs) + 1)) .. 0])) / (fromIntegral(length xs + 1) / 2)
 
 {-
 
