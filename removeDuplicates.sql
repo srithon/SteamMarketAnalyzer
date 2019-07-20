81e9f022f60fe344400b73ec7f60e6b364185ba3
Sri T <sri7thon@gmail.com>
2019-07-20 12:17:49 -0700

    Edited script; PostgreSQL doesnt allow CTE DELETE

--- removeDuplicates.sql
+++ removeDuplicates.sql
@@ -1,5 +1,7 @@
-WITH ItemNamesCTE AS (
-	SELECT *, ROW_NUMBER() OVER (PARTITION BY name ORDER BY name) AS RowNum
-	from tf2_item_names
-)
-DELETE FROM ItemNamesCTE WHERE RowNum > 1;
+WITH deleted AS
+(DELETE FROM tf2_item_names returning *),
+inserted AS
+(SELECT name, ROW_NUMBER() OVER (PARTITION BY name ORDER BY name) RowNum
+	FROM deleted)
+INSERT INTO tf2_item_names SELECT name FROM inserted
+WHERE RowNum = 1;
