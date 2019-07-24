fd0685d5d2401773fa19d780c0f5ce24a3991d51
Sridaran <sri7thon@gmail.com>
2019-07-23 21:27:03 -0400

    Working HintTextEntry

--- gui/Main.py
+++ gui/Main.py
@@ -38,13 +38,44 @@ class FillDBFrame(tk.Frame):
         tk.Frame.__init__(self, master)
         self.populate()
         self.grid(column=0, row=0, sticky='nsew')
+        # num_workers, appid, output_table_name, input_table_name
 
     def populate(self):
         self.start_button = tk.Button(self, text='Start', command=self.start_analysis)
         self.start_button.grid(padx=50, pady=50)
+
+        self.num_workers_field = HintTextEntry(self, 'Number of Workers', grid_args={'padx':50,'pady':20})
+        self.appid_field = HintTextEntry(self, 'App ID', grid_args={'padx':50,'pady':20})
+        self.output_table_name_field = HintTextEntry(self, 'Output Table', grid_args={'padx':50,'pady':20})
+        self.input_table_name_field = HintTextEntry(self, 'Input Table', grid_args={'padx':50,'pady':20})
     
     def start_analysis(self):
-        print('Start filldb....')
+        print('Start filldb?')
+        print(self.num_workers_field.get(0))
+        print(self.appid_field.get(0))
+        print(self.output_table_name_field.get(0))
+        print(self.input_table_name_field.get(0))
+
+class HintTextEntry(tk.Text):
+    def __init__(self, master, hint_text, grid_args=None):
+        tk.Text.__init__(self, master)
+        self.hint_text = hint_text
+        self.insert('1.0', hint_text)
+        self.bind('<Enter>', self.handle_enter)
+        self.bind('<Leave>', self.handle_leave)
+
+        if grid_args is not None:
+            self.grid(**grid_args)
+    
+    def handle_enter(self, event):
+        print(self.get(1.0,'end'))
+        print(self.hint_text)
+        if self.get(1.0,'end').rstrip() == self.hint_text:
+            self.delete(1.0, 'end')
+
+    def handle_leave(self, event):
+        if not self.get(1.0, 'end').rstrip():
+            self.insert(1.0, self.hint_text)
 
 class MainFrame(tk.Frame):
     def __init__(self, master):
@@ -59,6 +90,14 @@ class MainFrame(tk.Frame):
         self.filldb_button.grid(padx=50, pady=50)
         self.analysis_button = tk.Button(self, text='Analysis', command=self.master.switch_to_analysis)
         self.analysis_button.grid(padx=50, pady=50)
+
+class Logs(tk.Text):
+    def __init__(self, master):
+        tk.Text.__init__(self, master)
+        self.grid()
+    
+    def append(self, text):
+        self.insert('end', text)
 
 root = tk.Tk()
 app = Application(root)
