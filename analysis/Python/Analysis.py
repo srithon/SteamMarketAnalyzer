fc009190f94ab715a4e6638f562bfcdb91bb0fdd
Sri T <sri7thon@gmail.com>
2019-08-02 13:39:28 -0700

    Plots all shortlisted items in separate windows

--- analysis/Python/Analysis.py
+++ analysis/Python/Analysis.py
@@ -1,22 +1,55 @@
 from Trend import *
 from enum import Enum
 
+from matplotlib import pyplot as plt
+
 class Action(Enum):
     BUY, SELL, IGNORE = range(3)
 
-def suggested_action(volume, prices, fluctuation=None):
-    if (volume < 8):
+def suggested_action(prices, fluctuation=None):
+    direction = fluctuationDirection(fluctuation) if fluctuation is not None else fluctuation(prices)
+    if direction == None:
         return Action.IGNORE
+    elif direction == Direction.UP:
+        return Action.SELL
     else:
-        if prices[0] < 25 or prices[0] > 80:
-            return Action.IGNORE
-        direction = fluctuationDirection(fluctuation) if fluctuation is not None else fluctuation(prices)
-        if direction == None:
-            return Action.IGNORE
-        elif direction == Direction.UP:
-            return Action.SELL
-        else:
-            return Action.BUY
+        return Action.BUY
+
+def check_volume(volume):
+    return volume > 8
+
+def check_price(price):
+    return price < 80 and price > 25
+
+"""
+Passed in as
+( item_name, fluctuation, prices, volume, times, action )
+
+GOAL
+Be able to switch between different plots
+
+"""
+def plot_items(tuple_list=None):
+    plt.style.use('fivethirtyeight')
+    figures = list()
+
+    for tup in tuple_list:
+        fig = plt.figure(tup[0])
+        
+        plt.plot(tup[4], tup[2], label=tup[0], linewidth=2)
+
+        plt.ylim([0, 100])
+
+        # plt.xticks(rotation=90)
+
+        fig.autofmt_xdate()
+
+        plt.xlabel('Time Recorded')
+        plt.ylabel('Price (USD)')
+
+        figures.append(fig)
+    
+    plt.show()
 
 """
 suggestedAction volume (x:xs)
--- analysis/Python/Driver.py
+++ analysis/Python/Driver.py
@@ -4,7 +4,6 @@ import psycopg2
 
 table_name = 'csgo'
 
-
 def absolute_value_of_nth_element_in_tuple_callable(n):
 	def g(tuple):
 		return abs(tuple[n])
@@ -31,20 +30,24 @@ Plot the top n items
 shortlist = list()
 
 for item in item_names:
-	cursor.execute(f'SELECT volume, price FROM {table_name} WHERE name=%s ORDER BY time DESC', (item,))
+	cursor.execute(f'SELECT volume, price, time FROM {table_name} WHERE name=%s ORDER BY time DESC', (item,))
 	entries = cursor.fetchall()
 
-	prices = [entry[1] for entry in entries]
 	volume = entries[0][0]
 
-	fluc = Trend.fluctuationFirstToRest(prices)
-	action = Analysis.suggested_action(volume, prices, fluc)
+	if Analysis.check_volume(volume) and Analysis.check_price(entries[0][1]):
+		prices = [entry[1] for entry in entries]
 
-	if action != Analysis.Action.IGNORE:
-		shortlist.append( (item, fluc, prices, volume, action) )
+		fluc = Trend.fluctuationFirstToRest(prices)
+		action = Analysis.suggested_action(prices, fluc)
+
+		if action != Analysis.Action.IGNORE:
+			shortlist.append( (item, fluc, prices, volume, [entry[2] for entry in entries], action) )
 
 shortlist.sort(key=absolute_value_of_nth_element_in_tuple_callable(1), reverse=True)
 
 [print(f'{row[0]}:{row[-1]}:{row[1]}') for row in shortlist]
+
+Analysis.plot_items(shortlist)
 
 # print (Analysis.suggested_action(8,[4, 8, 6, 4, 2, 0]))
